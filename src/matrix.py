d1672a63d23d139e3c6577f418cb61a31eb2a006
Andreas Pfeil <andreas.pfeil@student.kit.edu>
2018-06-24 15:07:55 +0200

    Finished

--- src/matrix.py
+++ src/matrix.py
@@ -11,45 +11,50 @@ def invert(imagem):
 path = "/home/verpfeilt/Git/Scion/Brightfield-time-series-proliferation/darktable_exported/"
 
 def sobelThresh(img2):
-    img2 = cv.GaussianBlur(img2, (25,25), 0)
+    img2 = cv.GaussianBlur(img2, (25,25), 20)
 
-    img2_x = cv.Sobel(img2,cv.CV_64F,1,0,ksize=5)
-    img2_y = cv.Sobel(img2,cv.CV_64F,0,1,ksize=5)
-    img2_xy = np.abs(img2_x) + np.abs(img2_y)
+    #img2_x = cv.Sobel(img2,cv.CV_64F,1,0,ksize=5)
+    #img2_y = cv.Sobel(img2,cv.CV_64F,0,1,ksize=5)
+    #img2_xy = np.abs(img2_x) + np.abs(img2_y)
+    img2_xy = cv.Canny(img2, 1, 6)
 
+    img2_xy = np.array(img2_xy)
     img2_xy = invert(img2_xy)
 
-    width, height, channels = img2_xy.shape
-    thresh = 184
-    ret,img2_xy = cv.threshold(img2_xy, thresh, 255,  cv.THRESH_BINARY)
+    #width, height = img2_xy.shape
+    #thresh = 184
+    #ret,img2_xy = cv.threshold(img2_xy, thresh, 255,  cv.THRESH_BINARY)
 
     kernel = np.ones((3,3),np.uint8)
-    #img2_xy = cv.erode(img2_xy,kernel,iterations = 5)
-    img2_xy = cv.morphologyEx(img2_xy, cv.MORPH_OPEN, kernel, iterations = 6)
+    img2_xy = cv.erode(img2_xy,kernel,iterations = 3)
+    #img2_xy = cv.morphologyEx(img2_xy, cv.MORPH_OPEN, kernel, iterations = 1)
+    #cv.namedWindow('image', cv.WINDOW_NORMAL)
+    #cv.imshow('image',img2_xy)
+    #cv.waitKey(0)
     return img2_xy
 
 def compute_cells(image):
     counter = 0
-    width, height, channels = image.shape
+    width, height = image.shape
     for x in range(width):
         for y in range(height):
-            if image[x,y,0] < 200:
+            if image[x,y] < 200:
                 counter += 1
-    return counter
+    return counter * 1.83 * 1.83
 
 def plot_timeline(cellcount):
     t = np.arange(0, len(cellcount), 1)
     s = cellcount
     plot(t, s)
 
-    xlabel('Zeit')
-    ylabel('Zellflaeche')
+    xlabel('Aufnahmen')
+    ylabel('Zellflaeche (mikrometer)')
     title('Zellwachstum')
     grid(True)
     show()
 
 cellcount = []
-for i in range(1, 98, 10):
+for i in range(1, 98, 1):
     #print path + "Brightfield-time-series-proliferation_t{:02d}.tif".format(i)
     print i
     image = sobelThresh(cv.imread(path + "Brightfield-time-series-proliferation_t{:02d}.tif".format(i)))
